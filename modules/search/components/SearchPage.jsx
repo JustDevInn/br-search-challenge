5266d1bc4bd7df4bd959d7ca340d4531a4423778
Justin Peeters <justinpeeters@MacBook-Pro-van-Justin.local>
2025-03-11 09:33:55 +0800

    core task 1, list of API results

--- modules/search/components/SearchPage.jsx
+++ modules/search/components/SearchPage.jsx
@@ -2,7 +2,23 @@ const SearchPage = ({ searchResults, searchState }) => {
   console.log('SearchPage Props', searchResults, searchState)
 
   return (
-    <div className='max-w-5xl mx-auto'>
+    <div className='max-w-5xl mx-auto px-4'>
+      <h1 className="text-2xl font-semibold my-4">Retreats</h1>
+      {/* {/* Create a UL to display the searchResults. Map over searchResults.
+      hits to create li for each result. 
+      if its empty, display a h2 no results found.  */}
+      {searchResults?.hits?.length > 0 ? (
+        <ul className="p20">
+          {searchResults.hits.map((retreat) => (
+            <li key={retreat.id} className="p2">
+              <h2 className="font-bold underline">{retreat.name}</h2>
+            </li>
+          ))}
+        </ul>
+      ) :
+      (
+        <p>No results found.</p>
+      )}
     </div>
   )
 }
